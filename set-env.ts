b6bd6aed7d69e6cde6ccb3ace841724648c4295d
Adrian116-code <delapuenteadrian3@gmail.com>
2026-02-03 23:59:02 -0500

    Update set-env.ts

--- set-env.ts
+++ set-env.ts
@@ -3,8 +3,8 @@ const { writeFile } = require('fs');
 const envConfigFile = `
 export const environment = {
   production: true,
-  supabaseUrl: '${process.env['https://ghgpkpsbfimmpuxvkfqt.supabase.co'] || ''}',
-  supabaseKey: '${process.env['sb_publishable_A2A1vOreY9nfUTbxK479bw_7wGCzIan'] || ''}'
+  supabaseUrl: '${process.env['https://ghgpkpsbfimmpuxvkfqt.supabase.co']}',
+  supabaseKey: '${process.env['sb_publishable_A2A1vOreY9nfUTbxK479bw_7wGCzIan']}'
 };
 `;
 
