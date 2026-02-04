6544a423ed3b5add83953028fc1fa278565d4dbd
Adrian116-code <delapuenteadrian3@gmail.com>
2026-02-04 12:02:50 -0500

    Create config-env.js

---
+++ config-env.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+// Definimos la ruta donde Angular espera encontrar el archivo
+const dir = './src/environments';
+const targetPath = path.join(dir, 'environment.ts');
+
+// Si no existe la carpeta, la creamos
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+
+// Contenido del archivo usando las variables que pondremos en Cloudflare
+// Recordamos que el URL es string
+const envConfigFile = `
+export const environment = {
+  production: true,
+  supabaseUrl: '${process.env.SUPABASE_URL || ""}',
+  supabaseKey: '${process.env.SUPABASE_KEY || ""}'
+};
+`;
+
+console.log('Generando archivo de entorno en: ' + targetPath);
+
+fs.writeFileSync(targetPath, envConfigFile, 'utf8');
